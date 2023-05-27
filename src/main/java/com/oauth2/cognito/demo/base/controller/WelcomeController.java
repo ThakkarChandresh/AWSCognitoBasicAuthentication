85586236983a428d5dd9dc1f0d5a1874dfbc2920
Chandresh Thakkar <tony051970@gmail.com>
2023-05-27 10:16:11 +0530

    Get User Information

--- src/main/java/com/oauth2/cognito/demo/base/controller/WelcomeController.java
+++ src/main/java/com/oauth2/cognito/demo/base/controller/WelcomeController.java
@@ -2,18 +2,29 @@ package com.oauth2.cognito.demo.base.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
+import java.util.Map;
 
 @RestController
 public class WelcomeController {
 
     @GetMapping("/")
-    public ResponseEntity<String> home(Principal principal) {
+    public ResponseEntity<String> home(Authentication authentication) {
+        boolean isAuthenticated = authentication.isAuthenticated();
 
-        return new ResponseEntity<>("Welcome Dear You Are Authenticated!" + principal.getName(), HttpStatus.OK);
+        ResponseEntity<String> entity;
+        if (isAuthenticated) {
+            DefaultOidcUser defaultOidcUser = (DefaultOidcUser) authentication.getPrincipal();
+            Map<String, Object> userAttributes = defaultOidcUser.getAttributes();
 
+            entity = new ResponseEntity<>("Welcome Dear " + userAttributes.get("cognito:username") + " You Are Authenticated!", HttpStatus.OK);
+        } else {
+            entity = new ResponseEntity<>("Not Authenticated!", HttpStatus.UNAUTHORIZED);
+        }
+        return entity;
     }
 }
